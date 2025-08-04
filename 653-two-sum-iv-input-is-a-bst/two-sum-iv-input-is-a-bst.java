c138eeb3411bcac5375e030d0272021051c96268
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-04 13:22:05 +0530

    Time: 3 ms (63.59%) | Memory: 44.9 MB (84.93%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,25 +14,19 @@
  * }
  */
 class Solution {
+    HashSet<Integer> set = new HashSet<>();
     public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> list = new ArrayList<>();
-        inor(root,list);
-        System.out.println(list);
-        int l=0,r=list.size()-1;
-        while(l<r){
-            if((list.get(l)+list.get(r))==k)  return true;
-            else if(list.get(l)+list.get(r)>k)  r--;
-            else    l++;
-        }
-        
-        return false;
+        return inorder(root, k);
     }
-    void inor(TreeNode node,ArrayList<Integer> list){
-        if(node==null){
-            return;
-        }
-        inor(node.left,list);
-        list.add(node.val);
-        inor(node.right,list);
+
+    private boolean inorder(TreeNode node, int k) {
+        if (node == null) return false;
+
+        // if (inorder(node.left, k)) return true;
+
+        if (set.contains(k - node.val)) return true;
+        set.add(node.val);
+
+        return inorder(node.left, k)||inorder(node.right, k);
     }
 }
