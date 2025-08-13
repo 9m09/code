017c1162d73d47e6d9aff35613a603125ca77d75
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-13 11:40:00 +0530

    Time: 12 ms (75.73%) | Memory: 45.5 MB (11.82%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -11,16 +11,16 @@ class Disjoint{
             size[i]=1;
         }
     }
-    public int findUPar(int node) {
+    public int fup(int node) {
         if (node == parent[node]) {
             return node;
         }
-        parent[node] = findUPar(parent[node]); 
+        parent[node] = fup(parent[node]); 
         return parent[node];
     }
     public void ubr(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
+        int ulp_u = fup(u);
+        int ulp_v = fup(v);
         if (ulp_u == ulp_v) return;
 
         if (rank[ulp_u] < rank[ulp_v]) {
@@ -33,8 +33,8 @@ class Disjoint{
         }
     }
     public void ubs(int u,int v){
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
+        int ulp_u = fup(u);
+        int ulp_v = fup(v);
         if (ulp_u == ulp_v) return;
         
         if (size[ulp_u] < size[ulp_v]) {
@@ -51,25 +51,60 @@ class Disjoint{
 }
 class Solution {
     public int removeStones(int[][] stones) {
-        int con=0;
-        int r=Integer.MIN_VALUE;
-        int c=Integer.MIN_VALUE;
-        for(int i=0;i<stones.length;i++){
-            r=Math.max(r,stones[i][0]);
-            c=Math.max(c,stones[i][1]);
+        Disjoint r = new Disjoint(stones.length);
+        Disjoint c = new Disjoint(stones.length);
+        HashMap<Integer,Integer> rmap = new HashMap<>();
+        HashMap<Integer,Integer> cmap = new HashMap<>();
+
+        rmap.put(stones[0][0],0);
+        cmap.put(stones[0][1],0);
+        for(int i=1;i<stones.length;i++){
+            if(rmap.containsKey(stones[i][0])&&cmap.containsKey(stones[i][1])){
+                int pr=r.fup(rmap.get(stones[i][0]));
+                int pc=c.fup(cmap.get(stones[i][1]));
+                r.ubr(pr,pc);c.ubr(pr,pc);
+                r.ubr(pr,i);c.ubr(pr,i);
+            }
+            else if(rmap.containsKey(stones[i][0])){
+                int pr=r.fup(rmap.get(stones[i][0]));
+                r.ubr(pr,i);c.ubr(pr,i);
+                cmap.put(stones[i][1],i);
+            }
+            else if(cmap.containsKey(stones[i][1])){
+                int pc=c.fup(cmap.get(stones[i][1]));
+                r.ubr(pc,i);c.ubr(pc,i);
+                rmap.put(stones[i][0],i);
+            }
+            else{
+                rmap.put(stones[i][0],i);
+                cmap.put(stones[i][1],i);
+            }
         }
-        Disjoint ds = new Disjoint(r+c+2);
-        HashSet<Integer> set = new HashSet<>();
+        int rem=0;
         for(int i=0;i<stones.length;i++){
-            int col=r+1+stones[i][1];
-            ds.ubs(stones[i][0],col);
-            set.add(col);
-            set.add(stones[i][0]);
+            if(i!=r.fup(i))   rem++;
         }
+        return rem;
+
+        // int con=0;
+        // int r=Integer.MIN_VALUE;
+        // int c=Integer.MIN_VALUE;
+        // for(int i=0;i<stones.length;i++){
+        //     r=Math.max(r,stones[i][0]);
+        //     c=Math.max(c,stones[i][1]);
+        // }
+        // Disjoint ds = new Disjoint(r+c+2);
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i=0;i<stones.length;i++){
+        //     int col=r+1+stones[i][1];
+        //     ds.ubs(stones[i][0],col);
+        //     set.add(col);
+        //     set.add(stones[i][0]);
+        // }
         
-        for(int i:set){
-            if(ds.parent[i]==i) con++;
-        }
-        return stones.length-con;
+        // for(int i:set){
+        //     if(ds.parent[i]==i) con++;
+        // }
+        // return stones.length-con;
     }
 }
