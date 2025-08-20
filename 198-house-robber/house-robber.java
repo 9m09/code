4c2327a5915bbf72af93178d69bee7275650ab3b
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-21 01:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (15.21%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,39 @@
+// class Solution {
+//     int max=Integer.MIN_VALUE;
+//     public int rob(int[] nums) {
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, -1);
+//         help(nums,Integer.MIN_VALUE,0,0);
+//         return max;
+//     }
+//     void help(int[] arr,int cmax,int i,int sum){
+// 	    if(i>=arr.length){
+//             if(i<arr.length)
+//             sum=sum+arr[i];
+// 	        max=Math.max(max,sum);			
+// 			return;												
+// 		}									
+
+// 	    help(arr,cmax,i+2,sum+arr[i]);
+//         help(arr,cmax,i+1,sum);
+//     }
+// }
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, dp);
+    }
+
+    int solve(int[] arr, int i, int[] dp) {
+        if (i >= arr.length) return 0;
+        if (dp[i] != -1) return dp[i]; 
+        
+        int non = arr[i] + solve(arr, i + 2, dp);
+        int adj = solve(arr, i + 1, dp);
+
+        return dp[i] = Math.max(non, adj);
+    }
+}
