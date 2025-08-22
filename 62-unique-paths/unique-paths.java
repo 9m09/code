b2ef8486f1cb97b137905d0c9ec707d937d345cd
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-22 23:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (60.76%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==1)    return 1;
+        int[][] grid=new int[m][n];
+        for(int j=0;j<n;j++)    grid[1][j]=j+1;
+        for(int i=2;i<m;i++){
+            grid[i][0]=1;
+            for(int j=1;j<n;j++){
+                grid[i][j]=grid[i][j-1]+grid[i-1][j];
+            }
+        }
+
+        return  grid[m-1][n-1];
+    }
+}
