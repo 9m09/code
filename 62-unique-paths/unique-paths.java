dcf6c29a68d19605c45f87eade787fe51a4f9ea3
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-23 00:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (35.66%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -12,21 +12,44 @@ class Solution {
 
     //     return  grid[m-1][n-1];
     // }
+
+    //memoization
+    // public int uniquePaths(int m, int n){
+    //     if(m==1)    return 1;
+    //     int[][] dp = new int[m][n];
+    //     for(int[] dpp:dp)
+    //     Arrays.fill(dpp,-1);
+    //     return help(dp,m-1,n-1);
+    // }
+    // int help(int[][] dp,int i,int j){
+    //     if(i==0&&j==0)  return 1;
+    //     if(i<0||j<0)    return 0;
+    //     if(dp[i][j]!=-1)    return dp[i][j];
+
+    //     int up=help(dp,i-1,j);
+    //     int left=help(dp,i,j-1);
+
+    //     return dp[i][j]=left+up;
+    // }
+
+    //tabulation
     public int uniquePaths(int m, int n){
         if(m==1)    return 1;
+    
         int[][] dp = new int[m][n];
-        for(int[] dpp:dp)
-        Arrays.fill(dpp,-1);
-        return help(dp,m-1,n-1);
-    }
-    int help(int[][] dp,int i,int j){
-        if(i==0&&j==0)  return 1;
-        if(i<0||j<0)    return 0;
-        if(dp[i][j]!=-1)    return dp[i][j];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&j==0)   dp[0][0]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i!=0)    up=dp[i-1][j];
+                    if(j!=0)    left=dp[i][j-1];
+                    dp[i][j]=left+up;
+                }
+            }
+        }
 
-        int up=help(dp,i-1,j);
-        int left=help(dp,i,j-1);
-
-        return dp[i][j]=left+up;
+        return dp[m-1][n-1];
     }
 }
