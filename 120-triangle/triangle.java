9352bc6373119baaa058b0edc6e6fbbc670b4c41
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-23 17:48:05 +0530

    Time: 6 ms (20.45%) | Memory: 45.1 MB (57.27%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,28 +1,26 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> tr) {
         int[][] dp = new int[tr.size()][];
-
         for(int i=0;i<tr.size();i++){
             dp[i]=new int[tr.get(i).size()];
-            Arrays.fill(dp[i],-1);
         }
+
+        dp[0][0]=tr.get(0).get(0);
+        for(int i=1;i<tr.size();i++){
+            for(int j=0;j<tr.get(i).size();j++){
+                int up=Integer.MAX_VALUE;
+                int di=Integer.MAX_VALUE;
+                if(j<dp[i-1].length)    up=dp[i-1][j]; 
+                if(j!=0)    di=dp[i-1][j-1];
+                int min=Math.min(up,di);
+                dp[i][j]=(min==Integer.MAX_VALUE?0:min)+tr.get(i).get(j);
+            }
+        } 
+
         int min=Integer.MAX_VALUE;
-        for(int j=tr.get(tr.size()-1).size()-1;j>=0;j--)   
-            min=Math.min(help(dp,tr,tr.size()-1,j),min);
-           
-        return min;   
-    }
-    int help(int[][] dp , List<List<Integer>> tr,int i, int j){
-        if(i==0&&j==0)  return dp[0][0]=tr.get(0).get(0);
-        // if(i<0||j<0)    return Integer.MAX_VALUE;
-        if (i < 0 || j < 0 || j >= tr.get(i).size()) return Integer.MAX_VALUE;
-
-        if(dp[i][j]!=-1)    return dp[i][j];
-
-        int up=help(dp,tr,i-1,j-1);
-        int left=help(dp,tr,i-1,j);
-
-        return dp[i][j]=Math.min(up,left)+tr.get(i).get(j);
-
+        for(int j=0;j<dp[tr.size()-1].length;j++){
+            min=Math.min(min,dp[tr.size()-1][j]);
+        }
+        return min;
     }
 }
