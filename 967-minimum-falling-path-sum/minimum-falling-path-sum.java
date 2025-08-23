1f40a311ce3a96b6f284a90c6fb329e1c6c22133
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-23 23:59:15 +0530

    Time: 6 ms (70.82%) | Memory: 45.2 MB (79.34%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public int minFallingPathSum(int[][] mat) {
-        int[][] dp = new int[mat.length][mat[0].length];
-        for(int j=0;j<mat[0].length;j++)    dp[0][j]=mat[0][j];
-        for(int i=1;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                int ld=Integer.MAX_VALUE,rd=Integer.MAX_VALUE;
-                int up=dp[i-1][j];
-                if(j-1>=0)    ld=dp[i-1][j-1];
-                if(j+1<mat[0].length)    rd=dp[i-1][j+1];
-                dp[i][j]=Math.min(Math.min(up,ld),rd)+mat[i][j];
+        int n = mat.length, m = mat[0].length;
+        int[] prev = new int[m];
+        int[] curr = new int[m];
+
+        for (int j = 0; j < m; j++) prev[j] = mat[0][j];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int up = prev[j];
+                int ld = (j > 0) ? prev[j - 1] : Integer.MAX_VALUE;
+                int rd = (j < m - 1) ? prev[j + 1] : Integer.MAX_VALUE;
+                curr[j] = Math.min(up, Math.min(ld, rd)) + mat[i][j];
             }
+            prev = curr.clone();
         }
-        int min=Integer.MAX_VALUE;
-        for(int j=0;j<mat[0].length;j++)    min=Math.min(min,dp[mat.length-1][j]);
+
+        int min = Integer.MAX_VALUE;
+        for (int val : prev) min = Math.min(min, val);
         return min;
     }
 }
