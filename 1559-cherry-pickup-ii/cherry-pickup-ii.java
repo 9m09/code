5997b613525b57d84bd7a7b24add387ba606a823
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-25 00:12:23 +0530

    Time: 39 ms (15.93%) | Memory: 46.3 MB (31.90%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,27 +1,32 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
-        for(int[][] dpp:dp){
-            for(int[] dppp:dpp) Arrays.fill(dppp,-1);
-        }
-        return help(dp,grid,0,0,grid[0].length-1);    
-    }
-    int help(int[][][] dp,int[][] grid,int i,int j1,int j2){
-        if(j1<0||j1>=grid[0].length||j2<0||j2>=grid[0].length)  return 0;
-        if(i==grid.length-1){
-            if(j1==j2)  return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1)   return dp[i][j1][j2];
-        int max=Integer.MIN_VALUE;
-        int value;
-        for(int r1=-1;r1<=1;r1++){
-            for(int r2=-1;r2<=1;r2++){
-                if(j1==j2)  value=grid[i][j1];
-                else    value=grid[i][j1]+grid[i][j2];
-                max=Math.max(max,value+help(dp,grid,i+1,j1+r1,j2+r2));
+        int[][][] dp = new int [grid.length][grid[0].length][grid[0].length];
+        for(int j1=0;j1<grid[0].length;j1++){
+            for(int j2=0;j2<grid[0].length;j2++){
+                if(j1==j2)  dp[grid.length-1][j1][j2]=grid[grid.length-1][j2];
+                else    dp[grid.length-1][j1][j2]=grid[grid.length-1][j1]+grid[grid.length-1][j2];
             }
         }
-        return dp[i][j1][j2]=max;
+        for(int i=grid.length-2;i>=0;i--){
+            for (int j1 = 0; j1 < grid[0].length; j1++) {
+                for (int j2 = 0; j2 < grid[0].length; j2++) {
+                    int max=Integer.MIN_VALUE;
+                    for(int r1=-1;r1<=1;r1++){
+                        for(int r2=-1;r2<=1;r2++){
+                            int value;
+                            if(j1==j2)  value=grid[i][j1];
+                            else    value=grid[i][j1]+grid[i][j2];
+                            if ((j1 + r1 < 0 || j1 + r1 >= grid[0].length) || (j2 + r2 < 0 || j2 + r2 >= grid[0].length))
+                                max=Math.max(max,value);
+                            else    max=Math.max(max,value+dp[i+1][j1+r1][j2+r2]);
+
+                        }
+                    }
+                    dp[i][j1][j2]=max;
+                }
+            }
+        }
+
+        return dp[0][0][grid[0].length-1];
     }
 }
