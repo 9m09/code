7ddfb8ac5037064f8b36a8baeb345692ca3fc2e9
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-09-18 11:57:17 +0530

    Time: 19 ms (87.43%) | Memory: 51.1 MB (43.13%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,23 @@
-// shifting of indexes towards right
+import java.util.*;
+
 class Solution {
-    int l=0;
-
     public int longestCommonSubsequence(String t1, String t2) {
-        int[][] dp = new int[t1.length()+1][t2.length()+1];
-        for(int[] i: dp)    Arrays.fill(i,-1);
-        return help(t1, t1.length(), t2, t2.length(),dp);    
-    }
-
-    int help(String t1, int i1, String t2, int i2,int[][] dp){
+        int n = t1.length();
+        int m = t2.length();
         
-        if(i1==0||i2==0){
-            return 0;
+        int[][] dp = new int[n+1][m+1];
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (t1.charAt(i-1) == t2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
 
-        if(dp[i1][i2]!=-1)  return dp[i1][i2];
-
-        if(t1.charAt(i1-1)==t2.charAt(i2-1))    return dp[i1][i2]= 1 + help(t1, i1-1, t2, i2-1,dp);
-        // changed form i1 to i1-1 and i2 to i2-1 because we have shifted for the ease 
-        // but in strings s1 and s2 we have fixed size and it can give out of bound exception for first time
-
-        return dp[i1][i2]=Math.max(help(t1, i1-1, t2, i2,dp), help(t1, i1, t2, i2-1,dp));
+        return dp[n][m];
     }
 }
