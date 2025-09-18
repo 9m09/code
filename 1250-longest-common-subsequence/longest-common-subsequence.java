1060e47c5fc68783ad448e73ed36d80d90c5ec1d
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-09-18 11:54:48 +0530

    Time: 24 ms (32.40%) | Memory: 50.8 MB (68.74%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,21 +1,24 @@
+// shifting of indexes towards right
 class Solution {
     int l=0;
 
     public int longestCommonSubsequence(String t1, String t2) {
-        int[][] dp = new int[t1.length()][t2.length()];
+        int[][] dp = new int[t1.length()+1][t2.length()+1];
         for(int[] i: dp)    Arrays.fill(i,-1);
-        return help(t1, t1.length()-1, t2, t2.length()-1,dp);    
+        return help(t1, t1.length(), t2, t2.length(),dp);    
     }
 
     int help(String t1, int i1, String t2, int i2,int[][] dp){
         
-        if(i1==-1||i2==-1){
+        if(i1==0||i2==0){
             return 0;
         }
 
         if(dp[i1][i2]!=-1)  return dp[i1][i2];
 
-        if(t1.charAt(i1)==t2.charAt(i2))    return dp[i1][i2]= 1 + help(t1, i1-1, t2, i2-1,dp);
+        if(t1.charAt(i1-1)==t2.charAt(i2-1))    return dp[i1][i2]= 1 + help(t1, i1-1, t2, i2-1,dp);
+        // changed form i1 to i1-1 and i2 to i2-1 because we have shifted for the ease 
+        // but in strings s1 and s2 we have fixed size and it can give out of bound exception for first time
 
         return dp[i1][i2]=Math.max(help(t1, i1-1, t2, i2,dp), help(t1, i1, t2, i2-1,dp));
     }
