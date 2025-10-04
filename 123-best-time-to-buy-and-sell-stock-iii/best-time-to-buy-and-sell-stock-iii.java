d16412a1353d5e8b026eac608e854887737aade9
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-10-04 23:46:04 +0530

    Time: 115 ms (22.78%) | Memory: 75.1 MB (25.34%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,18 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy1 = -prices[0];
-        int sell1 = 0;
-        int buy2 = -prices[0];
-        int sell2 = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            int price = prices[i];
-            buy1 = Math.max(buy1, -price);
-            sell1 = Math.max(sell1, buy1 + price);
-            buy2 = Math.max(buy2, sell1 - price);
-            sell2 = Math.max(sell2, buy2 + price);
+        int[][][] dp = new int[prices.length][2][2];
+        for(int [][]dp1:dp){
+        for(int[] dpp:dp1)Arrays.fill(dpp,-1);
+        }
+        return help(prices,0,dp,0,0);    
+    }
+    int help(int[] p,int index,int[][][] dp,int buy,int count){ 
+        if(count==2)    return 0;
+        if(index==p.length)   return 0;
+        if(dp[index][buy][count]!=-1)   return dp[index][buy][count];
+        int max=Integer.MIN_VALUE;
+        if(buy==0){
+            int pp=-p[index]+help(p,index+1,dp,1,count);
+            int n=help(p,index+1,dp,buy,count);
+            return dp[index][buy][count]=Math.max(pp,n);
         }
 
-        return sell2;        
+            // int pp=Integer.MIN_VALUE;
+            // int c=p[index]-buy;
+            int pp=p[index]+help(p,index,dp,0,count+1);
+            int n=help(p,index+1,dp,buy,count);
+            max=Math.max(pp,n);
+            // return max;
+        return dp[index][buy][count]=max;
+
     }
 }
