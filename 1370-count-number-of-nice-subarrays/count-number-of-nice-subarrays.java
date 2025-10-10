a4a1ee7463bbe9480fa670a6cbe3674730301899
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-10-10 16:36:45 +0530

    Time: 13 ms (60.69%) | Memory: 55.1 MB (70.71%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,18 +1,15 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         // int odd=1;
-        int min=-1;
         Queue<Integer> q = new LinkedList<>();
         int i;
         for(i=0;i<nums.length;i++){
             if(nums[i]%2!=0)   {
-                min=i;
-                // q.add(i);
                 break;
             } 
         }
         
-        if(min==-1) return 0;
+        if(i==nums.length) return 0;
 
         int l=0,count=0;
         int odd=0;
@@ -27,20 +24,12 @@ class Solution {
                     odd--;
                 }
                 l++;
-                // System.out.print(l);
             }
             if(odd==k)  count=count+q.peek()-l+1;
             
         }
 
-        // for(;i<nums.length;i++){
-        //     if(nums[i]%2!=0)    odd++;
-        //     if(odd==k){
-        //         while(odd==k){
-        //             count=count+l;
-        //         }
-        //     }  
-        // }
+        
         return count;
     }
 }
